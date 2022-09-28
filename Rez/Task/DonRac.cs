ea12b7f4e4bde353f1d0926750aa6001237e418a
rezarria <votanamcb@outlook.com>
2022-09-28 20:03:52 +0700

    cập nhật dọn rác

--- Rez/Task/DonRac.cs
+++ Rez/Task/DonRac.cs
@@ -1,20 +1,27 @@
-﻿namespace Rez.Task;
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rez.Task;
 
 /// <summary>
 /// </summary>
 public class DonRac : IHostedService
 {
     private Timer _timer = null!;
-
+    private ILogger _logger;
+    public DonRac(ILogger<DonRac> logger)
+    {
+        _logger = logger;
+    }
+    
     /// <summary>
     /// </summary>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
-    public System.Threading.Tasks.Task StartAsync(CancellationToken cancellationToken)
+    public System.Threading.Tasks.Task StartAsync(CancellationToken cancellationToken = default)
     {
         _timer = new Timer(_ =>
         {
-            Console.WriteLine("QUET RAC....");
+            _logger.LogInformation("QUÉT RÁC....");
             GC.Collect();
         }, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
         return System.Threading.Tasks.Task.CompletedTask;
